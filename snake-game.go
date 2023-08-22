34ed6ad508abbfb02008fe38a67030c30c720fdc
betonomochalka <zxcnobrainn@gmail.com>
2023-08-22 11:49:04 +0200

    Make snake to grow | maxx 5

--- snake-game.go
+++ snake-game.go
@@ -2,26 +2,57 @@ package main
 
 import (
   "fmt"
+  "math/rand"
   "os"
   "os/exec"
-  "time"
   "sync"
-  "math/rand"
+  "time"
 
   term "github.com/nsf/termbox-go"
 )
 
 var (
-  lock = sync.Mutex{}
-  x = 1
-  y = 5
-  xApple int
-  yApple int
-  direction = "d"
-  coordinates[25][25] string
+  lock        = sync.Mutex{}
+  xApple      int
+  yApple      int
+  direction   = "d"
+  coordinates [25][25]string
+  ln          = 1
 )
 
-const(
+var (
+  x = 12
+  y = 12
+
+  oldX int
+  oldY int
+
+  x2 = x - 1
+  y2 = y
+
+  oldX2 int
+  oldY2 int
+
+  x3 = x - 2
+  y3 = y
+
+  oldX3 int
+  oldY3 int
+
+  x4 = x - 3
+  y4 = y
+
+  oldX4 int
+  oldY4 int
+
+  x5 = x - 4
+  y5 = y
+
+  oldX5 int
+  oldY5 int
+)
+
+const (
   snake = "[]"
   apple = "<>"
   empty = "　"
@@ -36,21 +67,26 @@ func clearCMD() {
 func takeInput() string {
   for {
     switch ev := term.PollEvent(); ev.Type {
-      case term.EventKey:
-        lock.Lock()
-        switch ev.Key {
-          case term.KeyArrowRight:
-            direction = "d"
-          case term.KeyArrowLeft:
-            direction = "a"
-          case term.KeyArrowDown:
-            direction = "s"
-          case term.KeyArrowUp:
-            direction = "w"
-        }
+    case term.EventKey:
+      lock.Lock()
+      switch ev.Key {
+      case term.KeyArrowRight:
+        direction = "d"
+      case term.KeyArrowLeft:
+        direction = "a"
+      case term.KeyArrowDown:
+        direction = "s"
+      case term.KeyArrowUp:
+        direction = "w"
+      }
     }
     lock.Unlock()
   }
+}
+
+func makeSnake() {
+  coordinates[y][x] = snake
+  coordinates[y2][x2] = snake
 }
 
 func drawField() {
@@ -63,24 +99,224 @@ func drawField() {
 }
 
 func moveSnake() {
-  oldx := x
-  oldy := y
-  lock.Lock()
-  switch direction {
-  case "w":
-    y--
-    coordinates[oldy][oldx] = empty
-  case "a":
-    x--
-    coordinates[oldy][oldx] = empty
-  case "s":
-    y++
-    coordinates[oldy][oldx] = empty
-  case "d":
-    x++
-    coordinates[oldy][oldx] = empty
+  if ln == 2 {
+    lock.Lock()
+    if direction == "w" {
+      oldX, oldY = x, y
+      y--
+      coordinates[y][x] = snake
+      oldX2, oldY2 = x2, y2
+      x2, y2 = oldX, oldY
+      coordinates[y2][x2] = snake
+      coordinates[oldY2][oldX2] = empty
+    } else if direction == "d" {
+      oldX, oldY = x, y
+      x++
+      coordinates[y][x] = snake
+      oldX2, oldY2 = x2, y2
+      x2, y2 = oldX, oldY
+      coordinates[y2][x2] = snake
+      coordinates[oldY2][oldX2] = empty
+    } else if direction == "a" {
+      oldX, oldY = x, y
+      x--
+      coordinates[y][x] = snake
+      oldX2, oldY2 = x2, y2
+      x2, y2 = oldX, oldY
+      coordinates[y2][x2] = snake
+      coordinates[oldY2][oldX2] = empty
+    } else if direction == "s" {
+      oldX, oldY = x, y
+      y++
+      coordinates[y][x] = snake
+      oldX2, oldY2 = x2, y2
+      x2, y2 = oldX, oldY
+      coordinates[y2][x2] = snake
+      coordinates[oldY2][oldX2] = empty
+    }
+
+    lock.Unlock()
+  } else if ln == 3 {
+    lock.Lock()
+    if direction == "w" {
+      oldX, oldY = x, y
+      y--
+      coordinates[y][x] = snake
+      oldX2, oldY2 = x2, y2
+      x2, y2 = oldX, oldY
+      coordinates[y2][x2] = snake
+      oldX3, oldY3 = x3, y3
+      x3, y3 = oldX2, oldY2
+      coordinates[y3][x3] = snake
+      coordinates[oldY3][oldX3] = empty
+    } else if direction == "d" {
+      oldX, oldY = x, y
+      x++
+      coordinates[y][x] = snake
+      oldX2, oldY2 = x2, y2
+      x2, y2 = oldX, oldY
+      coordinates[y2][x2] = snake
+      oldX3, oldY3 = x3, y3
+      x3, y3 = oldX2, oldY2
+      coordinates[y3][x3] = snake
+      coordinates[oldY3][oldX3] = empty
+    } else if direction == "a" {
+      oldX, oldY = x, y
+      x--
+      coordinates[y][x] = snake
+      oldX2, oldY2 = x2, y2
+      x2, y2 = oldX, oldY
+      coordinates[y2][x2] = snake
+      oldX3, oldY3 = x3, y3
+      x3, y3 = oldX2, oldY2
+      coordinates[y3][x3] = snake
+      coordinates[oldY3][oldX3] = empty
+    } else if direction == "s" {
+      oldX, oldY = x, y
+      y++
+      coordinates[y][x] = snake
+      oldX2, oldY2 = x2, y2
+      x2, y2 = oldX, oldY
+      coordinates[y2][x2] = snake
+      oldX3, oldY3 = x3, y3
+      x3, y3 = oldX2, oldY2
+      coordinates[y3][x3] = snake
+      coordinates[oldY3][oldX3] = empty
+    }
+    lock.Unlock()
+  } else if ln == 4 {
+    lock.Lock()
+    if direction == "w" {
+      oldX, oldY = x, y
+      y--
+      coordinates[y][x] = snake
+      oldX2, oldY2 = x2, y2
+      x2, y2 = oldX, oldY
+      coordinates[y2][x2] = snake
+      oldX3, oldY3 = x3, y3
+      x3, y3 = oldX2, oldY2
+      coordinates[y3][x3] = snake
+      oldX4, oldY4 = x4, y4
+      x4, y4 = oldX3, oldY3
+      coordinates[y4][x4] = snake
+      coordinates[oldY4][oldX4] = empty
+    } else if direction == "d" {
+      oldX, oldY = x, y
+      x++
+      coordinates[y][x] = snake
+      oldX2, oldY2 = x2, y2
+      x2, y2 = oldX, oldY
+      coordinates[y2][x2] = snake
+      oldX3, oldY3 = x3, y3
+      x3, y3 = oldX2, oldY2
+      coordinates[y3][x3] = snake
+      oldX4, oldY4 = x4, y4
+      x4, y4 = oldX3, oldY3
+      coordinates[y4][x4] = snake
+      coordinates[oldY4][oldX4] = empty
+    } else if direction == "a" {
+      oldX, oldY = x, y
+      x--
+      coordinates[y][x] = snake
+      oldX2, oldY2 = x2, y2
+      x2, y2 = oldX, oldY
+      coordinates[y2][x2] = snake
+      oldX3, oldY3 = x3, y3
+      x3, y3 = oldX2, oldY2
+      coordinates[y3][x3] = snake
+      oldX4, oldY4 = x4, y4
+      x4, y4 = oldX3, oldY3
+      coordinates[y4][x4] = snake
+      coordinates[oldY4][oldX4] = empty
+    } else if direction == "s" {
+      oldX, oldY = x, y
+      y++
+      coordinates[y][x] = snake
+      oldX2, oldY2 = x2, y2
+      x2, y2 = oldX, oldY
+      coordinates[y2][x2] = snake
+      oldX3, oldY3 = x3, y3
+      x3, y3 = oldX2, oldY2
+      coordinates[y3][x3] = snake
+      oldX4, oldY4 = x4, y4
+      x4, y4 = oldX3, oldY3
+      coordinates[y4][x4] = snake
+      coordinates[oldY4][oldX4] = empty
+    }
+    lock.Unlock()
+  } else if ln == 5 {
+    lock.Lock()
+    if direction == "w" {
+      oldX, oldY = x, y
+      y--
+      coordinates[y][x] = snake
+      oldX2, oldY2 = x2, y2
+      x2, y2 = oldX, oldY
+      coordinates[y2][x2] = snake
+      oldX3, oldY3 = x3, y3
+      x3, y3 = oldX2, oldY2
+      coordinates[y3][x3] = snake
+      oldX4, oldY4 = x4, y4
+      x4, y4 = oldX3, oldY3
+      coordinates[y4][x4] = snake
+      oldX5, oldY5 = x5, y5
+      x5, y5 = oldX4, oldY4
+      coordinates[y5][x5] = snake
+      coordinates[oldY5][oldX5] = empty
+    } else if direction == "d" {
+      oldX, oldY = x, y
+      x++
+      coordinates[y][x] = snake
+      oldX2, oldY2 = x2, y2
+      x2, y2 = oldX, oldY
+      coordinates[y2][x2] = snake
+      oldX3, oldY3 = x3, y3
+      x3, y3 = oldX2, oldY2
+      coordinates[y3][x3] = snake
+      oldX4, oldY4 = x4, y4
+      x4, y4 = oldX3, oldY3
+      coordinates[y4][x4] = snake
+      oldX5, oldY5 = x5, y5
+      x5, y5 = oldX4, oldY4
+      coordinates[y5][x5] = snake
+      coordinates[oldY5][oldX5] = empty
+    } else if direction == "a" {
+      oldX, oldY = x, y
+      x--
+      coordinates[y][x] = snake
+      oldX2, oldY2 = x2, y2
+      x2, y2 = oldX, oldY
+      coordinates[y2][x2] = snake
+      oldX3, oldY3 = x3, y3
+      x3, y3 = oldX2, oldY2
+      coordinates[y3][x3] = snake
+      oldX4, oldY4 = x4, y4
+      x4, y4 = oldX3, oldY3
+      coordinates[y4][x4] = snake
+      oldX5, oldY5 = x5, y5
+      x5, y5 = oldX4, oldY4
+      coordinates[y5][x5] = snake
+      coordinates[oldY5][oldX5] = empty
+    } else if direction == "s" {
+      oldX, oldY = x, y
+      y++
+      coordinates[y][x] = snake
+      oldX2, oldY2 = x2, y2
+      x2, y2 = oldX, oldY
+      coordinates[y2][x2] = snake
+      oldX3, oldY3 = x3, y3
+      x3, y3 = oldX2, oldY2
+      coordinates[y3][x3] = snake
+      oldX4, oldY4 = x4, y4
+      x4, y4 = oldX3, oldY3
+      coordinates[y4][x4] = snake
+      oldX5, oldY5 = x5, y5
+      x5, y5 = oldX4, oldY4
+      coordinates[y5][x5] = snake
+      coordinates[oldY5][oldX5] = empty
+    }
+    lock.Unlock()
   }
-  lock.Unlock()
 }
 
 func gameOver() {
@@ -89,16 +325,12 @@ func gameOver() {
   }
 }
 
-func drawSnake() {
-  coordinates[y][x] = snake
-}
-
 func makeField() {
   for i := 1; i < 24; i++ {
     for j := 1; j < 24; j++ {
       coordinates[i][j] = empty
     }
-  } 
+  }
 
   for i := 0; i < 25; i++ {
     for j := 1; j < 24; j++ {
@@ -114,38 +346,32 @@ func makeApple() {
   if coordinates[yApple][xApple] != apple {
     rand.Seed(time.Now().UnixNano())
 
-    xApple = rand.Intn(23-2)+1
-    yApple = rand.Intn(23-2)+1
-
-
-    if coordinates[yApple][xApple] == snake {
-      makeApple()
-      return
-    }
+    xApple = rand.Intn(23-2) + 1
+    yApple = rand.Intn(23-2) + 1
 
     coordinates[yApple][xApple] = apple
+
+    ln++
   }
 }
 
 func main() {
   duration := 17 * time.Millisecond
-  
+
   term.Init()
 
   clearCMD()
-
+  makeField()
+  makeSnake()
   go takeInput()
 
-  makeField()
-
-  for i := 0; ; i++{
+  for i := 10; ; i++ {
     clearCMD()
-    drawSnake()
-    drawField()
-    makeApple()
     if i%10 == 0 {
       moveSnake()
     }
+    drawField()
+    makeApple()
     gameOver()
 
     time.Sleep(duration)
